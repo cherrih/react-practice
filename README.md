194ef6c4d374303f98c126e69af8f44b8b483bcb
cherrih <cherri.hartigan@gmail.com>
2019-03-19 15:47:50 -0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # react-practice
+
+### Getting Started
+Run the following commands:
+
+- npm install
+- npm run build
+- npm start
+
+Navigate to localhost:3001
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-ReactDOM.render(<div>'Hello World'</div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello World!</div>, document.getElementById('app'));
